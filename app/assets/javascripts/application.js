728e4bdc9e02454cbc3b5a0d015ee2fb5976c248
関純一 <>
2018-04-01 16:11:00 +0900

    　sub-thubsumnail表示

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,10 +18,10 @@
 
 $(function(){
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-  $('form').on('change', 'input[type="file"]', function(e) {
+  $('.cover-image-upload').on('change', 'input[type="file"]', function(e) {
     var file = e.target.files[0],
         reader = new FileReader(),
-        $preview = $(".main-thumnail-preview");
+        $preview = $(".main-thumbnail-preview");
         t = this;
 
     // 画像ファイル以外の場合は何もしない
@@ -37,8 +37,40 @@ $(function(){
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
         $preview.append($('<img>').attr({
                   src: e.target.result,
-                  width: "150px",
-                  class: ".main-thumnail-preview",
+                  width: "350px",
+                  class: ".main-thumbnail-preview",
+                  title: file.name
+              }));
+      };
+    })(file);
+
+    reader.readAsDataURL(file);
+  });
+});
+
+$(function(){
+  //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
+  $('#prototype_captured_images_attributes_1_content').on('change', 'input[type="file"]', function(e) {
+    var file = e.target.files[0],
+        reader = new FileReader(),
+        $preview = $(".sub-thumbnail-preview");
+        t = this;
+
+    // 画像ファイル以外の場合は何もしない
+    if(file.type.indexOf("image") < 0){
+      return false;
+    }
+
+    // ファイル読み込みが完了した際のイベント登録
+    reader.onload = (function(file) {
+      return function(e) {
+        //既存のプレビューを削除
+        $preview.empty();
+        // .prevewの領域の中にロードした画像を表示するimageタグを追加
+        $preview.append($('<img>').attr({
+                  src: e.target.result,
+                  width: "100px",
+                  class: ".sub-thumbnail-preview",
                   title: file.name
               }));
       };
