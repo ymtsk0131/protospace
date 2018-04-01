c7fe84ca46b41226390973741e5ca8dfeb68de5c
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-01 15:53:56 +0900

    LIKE機能の実装完了

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,8 +11,18 @@ class LikesController < ApplicationController
     end
   end
 
+  def destroy
+    @like = Like.find(params[:id]).destroy
+    @count = Like.where("prototype_id = #{params[:prototype_id]}").count
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
-  
+
   def like_params
     params.permit(:prototype_id).merge(user_id: current_user.id)
   end
