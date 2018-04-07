233df74a4d7eb10159db7909680dd5b892a13735
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-07 15:17:45 +0900

    サブ画像のプレビューサイズを変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,21 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
+
 $(function(){
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
   $(document).on('change', '.cover-image-upload', function(e) {
@@ -56,10 +74,11 @@ function previewSubImage(label) {
       return function(e) {
         //既存のプレビューを削除
         $preview.empty();
+        // debugger;
         // .prevewの領域の中にロードした画像を表示するimageタグを追加
         $preview.append($('<img>').attr({
                   src: e.target.result,
-                  width: "100px",
+                  // width: "100%",
                   class: previewClassName,
                   title: file.name
               }));
