aa3aeb2fd16bd1197766ca8ff186849c04e6d0bb
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-04-07 14:57:30 +0900

    サブ画像のプレビュー表示完了

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,21 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require_tree .
-
 $(function(){
   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
   $(document).on('change', '.cover-image-upload', function(e) {
@@ -53,37 +35,6 @@ $(function() {
     previewSubImage(i);
   }
 });
-
-// $(function(){
-//   //画像ファイルプレビュー表示のイベント追加 fileを選択時に発火するイベントを登録
-//   $(document).on('change', '.image-upload-1', function(e) {
-//     var file = e.target.files[0],
-//         reader = new FileReader(),
-//         $preview = $(".sub-thumbnail-preview-1");
-//         t = this;
-//     // 画像ファイル以外の場合は何もしない
-//     if(file.type.indexOf("image") < 0){
-//       return false;
-//     }
-
-//     // ファイル読み込みが完了した際のイベント登録
-//     reader.onload = (function(file) {
-//       return function(e) {
-//         //既存のプレビューを削除
-//         $preview.empty();
-//         // .prevewの領域の中にロードした画像を表示するimageタグを追加
-//         $preview.append($('<img>').attr({
-//                   src: e.target.result,
-//                   width: "100px",
-//                   class: ".sub-thumbnail-preview-1",
-//                   title: file.name
-//               }));
-//       };
-//     })(file);
-
-//     reader.readAsDataURL(file);
-//   });
-// });
 
 function previewSubImage(label) {
   var eventClassName = ".image-upload-" + label;
