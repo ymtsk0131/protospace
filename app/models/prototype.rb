3bf8c875e50e72393ef1cfcf2e17b7286e069709
motosaka <ymtsk0131@live.jp>
2018-04-21 15:49:07 +0900

    dependent destroyを追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,8 +1,8 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :captured_images, dependent: :destroy
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   accepts_nested_attributes_for :captured_images, reject_if: :reject_sub_images
 
