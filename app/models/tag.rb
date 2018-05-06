8e299f3848bf1427cda979bb394512754f419a12
toshihide <get.enough.lactic.acid@gmail.com>
2018-05-06 15:27:33 +0900

    タグの部分テンプレートにタグを使用しているプロジェクト数を表示する機能を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,8 @@ class Tag < ActiveRecord::Base
   has_one :prototype, through: :prototype_tags
 
   validates :name, presence: true
+
+  def get_prototype_count
+    self.prototype_tags.count
+  end
 end
